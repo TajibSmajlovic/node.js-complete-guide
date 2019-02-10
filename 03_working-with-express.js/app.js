d69c0a2748d28e1018b9e7fbf827108f913a30e4
Tajib Smajlović <smajlovic.tajib@gmail.com>
2019-02-10 16:48:14 +0100

    adds middleware, handling-different-routes, parsing-incoming-requests and limiting-middleware-execution-to-post

---
+++ 03_working-with-express.js/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+/*app.use('/',(req, res, next) => {
+    console.log('This always runs!')
+    next()
+})*/
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+})
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express</h1>')
+})
+
+app.listen(3000)
