f5979d5734e490cd09843751649ed9bdc7399776
Tajib Smajlović <smajlovic.tajib@gmail.com>
2019-02-12 18:25:21 +0100

    adds styles for our pages and serving files statically

--- 03_working-with-express.js/app.js
+++ 03_working-with-express.js/app.js
@@ -1,12 +1,13 @@
 const path = require('path')
 
-const express = require("express");
+const express = require("express")
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
 const rootDir = require('./util/path')
 
-const app = express();
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
