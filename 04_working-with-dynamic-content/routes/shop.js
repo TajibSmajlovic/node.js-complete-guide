7a18a7cf864c66665933d3ea674a81becf80a20d
Tajib Smajlović <smajlovic.tajib@gmail.com>
2019-02-22 19:52:30 +0100

    Converting HTML files to .pug and adds pug Template

--- 04_working-with-dynamic-content/routes/shop.js
+++ 04_working-with-dynamic-content/routes/shop.js
@@ -10,7 +10,6 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   const products = adminData.products
   res.render('shop', {prods: products, docTitle: 'Shop'});
-
 })
 
 module.exports = router
