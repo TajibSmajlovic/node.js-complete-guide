4e693d69ea0e6c237f2f025e200ecd4b00b84c81
Tajib Smajlović <smajlovic.tajib@gmail.com>
2019-02-25 22:17:34 +0100

    adds controllers

---
+++ 05_the-model-view-controller-(mvc)/controllers/products.js
@@ -0,0 +1,31 @@
+const products = []
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formCSS: true,
+    productCSS: true,
+    ActiveAddProduct: true
+  })
+}
+
+exports.postAddProduct = (req, res, next) => {
+  products.push({title: req.body.title})
+  res.redirect('/')
+}
+
+exports.getProducts = (req, res, next) => {
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  })
+}
+
+exports.pageNotFound = (req, res, next) => {
+    res.status(404).render('404', {pageTitle: 404, pageContent: 'Page Not Found!!!'})
+}
